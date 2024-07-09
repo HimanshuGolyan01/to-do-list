0b6901d4ac298b06734164174f19986596d87ffe
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2024-07-09 12:48:03 +0530

    UI of ToDo List

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const toDoInput = document.getElementById("todo");
 const descriptionInput = document.getElementById("description");
 const button = document.getElementById("btn");
-const element = document.getElementById("main")
+const element = document.getElementById("todolist")
 
 button.addEventListener("click", function() {
     // console.log(toDoInput.value, descriptionInput.value)
+
     const div = document.createElement("div")
-    const todoInputText = document.createTextNode("todo : " + toDoInput.value)
-    const descriptionInputText = document.createTextNode("  desp: " + descriptionInput.value)
-    div.appendChild(todoInputText)
-    div.appendChild(descriptionInputText)
-    element.appendChild(div)
+    const todoInputText = document.createTextNode("TODO : " + toDoInput.value)
+    const descriptionInputText = document.createTextNode("  DESP: " + descriptionInput.value)
+
+    if(!toDoInput.value || !descriptionInput.value) {
+        alert("Please fill Todo and Desp");
+    }
+    else {
+        div.appendChild(todoInputText)
+        div.appendChild(descriptionInputText)
+        element.appendChild(div)
+        toDoInput.value = ""
+        descriptionInput.value = ""
+    }
+
+    
 })
+
 
